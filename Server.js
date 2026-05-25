897224e9648d75407cbb4bb07b82478858d198ec
fmoreira85 <fabiomoreiradacunha1@gmail.com>
2026-05-25 09:42:44 -0400

    node

--- Server.js
+++ Server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const Users = []
+
+app.post('/usuarios', function (req,res) {
+    Users.push(req.body)
+
+    res.send('Ok post')
+    
+})
 
 app.get('/usuarios', function (req,res) {
-    res.send('Ok,deu bom')
+    res.json(Users)
     
 })
 
